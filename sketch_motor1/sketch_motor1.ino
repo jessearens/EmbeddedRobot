7bb90f7ea58926f6bc61b72e9d9cb6786190fb27
jessearens <jesse.arens@hotmail.com>
2016-01-06 16:52:52 +0100

    Now the robot will stop when no message is received for 3 seconds, using an Arduino library to measure this time. TODO: let the robot stop when an object is sensed by its ultrasonic sensor.

--- sketch_motor1/sketch_motor1.ino
+++ sketch_motor1/sketch_motor1.ino
@@ -1,7 +1,9 @@
 
 #include <ros.h>
+
 #include <std_msgs/Empty.h>
 #include <geometry_msgs/Twist.h>
+#include <elapsedMillis.h>
 
 ros::NodeHandle  nh;
 //pins 1 are right motor, 2 is left
@@ -12,7 +14,10 @@ const int enablePin_2 = 25;
 const int reversePin_2 = 3;
 const int forwardPin_2 = 2;
 
+elapsedMillis timeElapsed;
+
 void MotormessageCb( const geometry_msgs::Twist& cmd_vel){
+  timeElapsed = 0; //Set timer to zero if a twist message is received.
   if (cmd_vel.angular.z != 0){
     if (cmd_vel.angular.z > 0){
       turnleft();
@@ -31,7 +36,6 @@ void MotormessageCb( const geometry_msgs::Twist& cmd_vel){
     }
   }
 }
-
 
 void turnleft(){
   digitalWrite(forwardPin_1,HIGH);
@@ -61,14 +65,12 @@ void movereverse(){
   digitalWrite(reversePin_2,LOW);
 }
 
-void DisablemessageCb( const std_msgs::Empty& stop_msg){
-  digitalWrite(reversePin_1,LOW);
+void Shutdown(){
+digitalWrite(reversePin_1,LOW);
 digitalWrite(reversePin_2,LOW);
 digitalWrite(forwardPin_1,LOW);
 digitalWrite(forwardPin_2,LOW);
 }
-
-ros::Subscriber<std_msgs::Empty> substop("stop_msg", &DisablemessageCb);
 
 ros::Subscriber<geometry_msgs::Twist> subtwist("cmd_vel", &MotormessageCb);
 
@@ -83,12 +85,12 @@ void setup()
   
   nh.initNode();
   nh.subscribe(subtwist);
-  nh.subscribe(substop);
 }
 
 void loop()
 { 
-  
-  nh.spinOnce();
-  delay(500);
+  if (timeElapsed > 3000){
+    Shutdown();
+  }
+    
 }
